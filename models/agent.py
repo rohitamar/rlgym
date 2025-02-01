7724f0860a7b350db923d7613335f7b8fddfdc54
Rohit <rohitamar@gmail.com>
2025-01-31 19:32:59 -0500

    ?

--- models/agent.py
+++ models/agent.py
@@ -1,13 +1,10 @@
 from abc import ABC, abstractmethod 
 
 class Agent(ABC):
-
-    @abstractmethod 
-    def act():
+    @abstractmethod
+    def act(self, state):
         pass 
 
     @abstractmethod 
-    def learn():
+    def learn(self):
         pass 
-
-    
