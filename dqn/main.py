c192ec58c7b94c61cdfff4d2a671ad522a07dabe
Rohit <rohitamar@gmail.com>
2025-08-02 23:45:22 -0400

    forgot to push dqn slight changes

---
+++ dqn/main.py
@@ -0,0 +1,19 @@
+from torch.utils.tensorboard import SummaryWriter
+import torch
+import gymnasium as gym
+from datetime import datetime
+
+from dqn.train_dqn import train_dqn
+
+timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+path = f"./runs/dqn-{timestamp}"
+writer = SummaryWriter(path)
+env = gym.make("CartPole-v1", render_mode='human')
+print(f"Target reward: {env.spec.reward_threshold}")
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Training models on {device}.")
+
+agent = train_dqn(env, device, writer)
+agent.save_weights(path)
+
